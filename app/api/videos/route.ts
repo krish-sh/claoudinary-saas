5d66bf3c01c0d8bb082825c076fbc2555a3d39c7
Krish Sharma <krishsharma3355@gmail.com>
2025-12-31 21:37:42 +0530

    calling fetch videos api

---
+++ app/api/videos/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "../../lib/prisma";
+
+export async function GET(request: NextRequest) {
+  try {
+    const videos = await prisma.video.findMany({
+      orderBy: { createdAt: "desc" },
+    });
+
+    return NextResponse.json(videos);
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error: "Error in fetching videos",
+      },
+      { status: 500 }
+    );
+  } finally {
+    await prisma.$disconnect();
+  }
+}
