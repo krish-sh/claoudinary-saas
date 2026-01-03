d98b5ef61626a61c3a330af785b1276caf66fa43
Krish Sharma <krishsharma3355@gmail.com>
2026-01-03 14:44:46 +0530

    Social-share UI part and logic build

--- app/api/image-uploader/route.ts
+++ app/api/image-uploader/route.ts
@@ -3,7 +3,7 @@ import { v2 as cloudinary } from "cloudinary";
 import { auth } from "@clerk/nextjs/server";
 
 cloudinary.config({
-  cloud_name: process.env.NEXT_PUBLIC_CLUDINARY_CLOUD_NAME,
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
--- app/api/video-uploader/route.ts
+++ app/api/video-uploader/route.ts
@@ -4,7 +4,7 @@ import { auth } from "@clerk/nextjs/server";
 import { prisma } from "../../lib/prisma";
 
 cloudinary.config({
-  cloud_name: process.env.NEXT_PUBLIC_CLUDINARY_CLOUD_NAME,
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
