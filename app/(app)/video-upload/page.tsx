2a1d509712b62ed172239ed90f2192eec97ca4ce
Krish Sharma <krishsharma3355@gmail.com>
2026-01-04 18:09:49 +0530

    Handling video

--- app/(app)/video-upload/page.tsx
+++ app/(app)/video-upload/page.tsx
@@ -1,5 +1,118 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { Fascinate } from "next/font/google";
 
 export default function VideUpload() {
-  return <div>VideUpload</div>;
+  const [file, setFile] = useState<File | null>(null);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [isUploading, setIsUploading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const route = useRouter();
+
+  // max file size 80 MB
+  const MAX_FILE_SIZE = 80 * 1024 * 1024;
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!file) return;
+
+    if (file.size > MAX_FILE_SIZE) {
+      setError(true);
+      true;
+    }
+
+    setIsUploading(true);
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("title", title);
+    formData.append("description", description);
+    formData.append("originalSize", file.size.toString());
+
+    try {
+      const response = await axios.post("/api/video/upload", formData);
+
+      if (response.status === 200) {
+        route.push("/success");
+      }
+    } catch (error) {
+      console.log("Error in VideoUpload", error);
+    } finally {
+      setIsUploading(false);
+    }
+  };
+
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-16  text-center ">Upload Video</h1>
+      <form onSubmit={handleSubmit} className="space-y-4">
+        <div>
+          <label className="label">
+            <span className="label-text mb-2">title</span>
+          </label>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="input input-info w-full mb-4"
+            required
+          />
+        </div>
+        <div>
+          <label className="label">
+            <span className="label-text mb-2">Description</span>
+          </label>
+          <textarea
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            className="textarea textarea-info w-full mb-4"
+            required
+          />
+        </div>
+        <div>
+          <label className="label">
+            <span className="label-text mb-2">Video File</span>
+          </label>
+          <input
+            type="file"
+            accept="video/*"
+            onChange={(e) => setFile(e.target.files?.[0] || null)}
+            className="file-input file-input-info w-full mb-4 "
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          className="btn btn-primary btn-outline"
+          disabled={isUploading}
+        >
+          {isUploading ? "Uploading..." : "Upload Video"}
+        </button>
+      </form>
+
+      {error && (
+        <div role="alert" className="alert alert-info">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="h-6 w-6 shrink-0 stroke-current"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            ></path>
+          </svg>
+          <span>File Size is too Large. Its likely to under the 100Mb</span>
+        </div>
+      )}
+    </div>
+  );
 }
