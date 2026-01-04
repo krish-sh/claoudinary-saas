1087bd616b9758b467d1da742f3b9533ff3a2a9b
Krish Sharma <krishsharma3355@gmail.com>
2026-01-04 23:37:03 +0530

    Video card making

--- app/(app)/video-upload/page.tsx
+++ app/(app)/video-upload/page.tsx
@@ -3,7 +3,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
-import { Fascinate } from "next/font/google";
 
 export default function VideUpload() {
   const [file, setFile] = useState<File | null>(null);
