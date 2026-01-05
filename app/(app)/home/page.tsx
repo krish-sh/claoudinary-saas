e2ee66be74e9c0c664417772f855d7a38824c8a3
Krish Sharma <krishsharma3355@gmail.com>
2026-01-05 22:52:38 +0530

    Fixing

--- app/(app)/home/page.tsx
+++ app/(app)/home/page.tsx
@@ -32,15 +32,13 @@ export default function Home() {
   }, [fetchVideos]);
 
   const handleDownload = useCallback((url: string, title: string) => {
-    () => {
-      const link = document.createElement("a");
-      link.href = url;
-      link.setAttribute("download", `${title}.mp4`);
-      link.setAttribute("target", "_blank");
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-    };
+    const link = document.createElement("a");
+    link.href = url;
+    link.setAttribute("download", `${title}.mp4`);
+    link.setAttribute("target", "_blank");
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
   }, []);
 
   if (loading) {
